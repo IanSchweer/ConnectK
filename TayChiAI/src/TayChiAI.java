e4b20e47afe45e6cfcabfe23c6db810f3a2a3b80
Ian Schweer <schweer@adobe.com>
2016-10-19 21:45:19 -0700

    Added state generation for gravity and non-gravity games

--- TayChiAI/src/TayChiAI.java
+++ TayChiAI/src/TayChiAI.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import connectK.CKPlayer;
 import connectK.BoardModel;
@@ -5,9 +6,11 @@ import java.awt.Point;
 
 public class TayChiAI extends CKPlayer {
 
+    private byte player;
 	public TayChiAI(byte player, BoardModel state) {
 		super(player, state);
 		teamName = "DummyAI";
+        player = player;
 	}
 
     private int getRandomInteger(int max) {
@@ -30,6 +33,33 @@ public class TayChiAI extends CKPlayer {
 
 	@Override
 	public Point getMove(BoardModel state, int deadline) {
+		ArrayList<BoardModel> states = this.getStates(state);
 		return getMove(state);
+	}
+	
+	private BoardModel generateState(BoardModel state, int x, int y) {
+		BoardModel newState = state.clone();
+		newState.placePiece(new Point(x, y), this.player);
+		return newState;
+	}
+	
+	private ArrayList<BoardModel> getStates(BoardModel state) {
+		// @todo: Add heuristic scoring.
+		ArrayList<BoardModel> states = new ArrayList<>();
+		if (state.gravityEnabled()) {
+			// the board will ensure points sink.
+			for (int i = 0; i < state.getWidth(); i++) {
+				states.add(this.generateState(state, i, state.getHeight() - 1));
+			}
+		} else {
+			// we can consider any move.
+			for (int i = 0; i < state.getWidth(); i++) {
+				for (int j = 0; j < state.getHeight(); j++) {
+					if (state.getSpace(i, j) == 0)
+						states.add(this.generateState(state, i, j));
+				}
+			}
+		}
+		return states;
 	}
 }
